07f4da2137840f9e3ba00f61d7eaca74661a5e8e
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-05-18 23:26:34 +0900

    #R003_Component 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import ImportConponent from "./R003_ImportConponent";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
+      <ImportConponent></ImportConponent>
     </div>
   );
 }
