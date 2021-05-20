db422553040621aeb4195ccc34db843b714653dd
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-05-20 21:01:44 +0900

    #R005_생명주기 함수 constructor(props) 사용

--- client/src/R003_ImportConponent.js
+++ client/src/R003_ImportConponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "React";
+import React, { Component } from "react";
 
 class R003_ImportConponent extends Component {
   render() {
---
+++ client/src/R005_LifecycleEx.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class R005_LifecycleEx extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    console.log("1. construcor Call");
+  }
+
+  render() {
+    console.log("3. render Call");
+    return <h2>[THIS IS CONSTRUCOR FUNCTION]</h2>;
+  }
+}
+
+export default R005_LifecycleEx;
