b6aa13fa784b20c23a0e1bdcd397b35c50d56a66
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-06-08 23:59:57 +0900

    #R020_props

---
+++ client/src/R20_PropsObjVal.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import datatype from "prop-types";
+
+class R20_PropsObjVal extends Component {
+  render() {
+    //     let {
+    // Object Json
+
+    //     } = this.props
+    return (
+      <div style={{ padding: "0px" }}>
+        {/* {JSON.stringify(Object Json)} */}
+      </div>
+    );
+  }
+}
+
+R20_PropsObjVal.propTypes = {
+  // Object Json: datatype.object,
+};
+
+export default R20_PropsObjVal;
