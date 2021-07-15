4fd25a46bcf84e4a1a88a544defd826a0d5a2575
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-07-15 17:08:08 +0900

    #R048_reactstrap Modal 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./App.css";
-import ReactstrapListGroup from "./R047_ReactstrapListGroup.js";
+import ReactstrapModa from "./R048_ReactstrapModal.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapListGroup />
+      <ReactstrapModa />
     </div>
   );
 }
