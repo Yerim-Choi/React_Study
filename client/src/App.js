9707fdf4cdb6bbd9eb6d26ad896b6163b1594dc6
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-07-17 23:24:11 +0900

    #R050_reactstrap Pagination 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./App.css";
-import ReactstrapNavbar from "./R049_ReactstrapNavbar.js";
+import ReactstrapPagination from "./R050_ReactstrapPagination.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapNavbar />
+      <ReactstrapPagination />
     </div>
   );
 }
