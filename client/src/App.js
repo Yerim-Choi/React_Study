677ec27c695ca3a55767d64d75c307661e95fbbe
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-07-26 23:21:20 +0900

    #R056_sweetalert2 Basic 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./App.css";
-import ReactstrapTab from "./R055_ReactstrapTab.js";
+import Sweetaler2Basic from "./R056_Sweetaler2Basic.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapTab />
+      <Sweetaler2Basic />
     </div>
   );
 }
