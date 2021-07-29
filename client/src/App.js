75420f672cef0cf62c6465bde352746a6cf0889a
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-07-29 14:47:09 +0900

    #R059_fetch get 호출

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 // import "./App.css";
-import Sweetalert2Confirm from "./R058_Sweetalert2Confirm.js";
-import "bootstrap/dist/css/bootstrap.css";
+import FetchGet from "./R059_FetchGet.js";
+// import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Sweetalert2Confirm />
+      <FetchGet />
     </div>
   );
 }
