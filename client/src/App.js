de2fc447254666b2e68a33212faeb231a7243b7e
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-03 16:28:45 +0900

    #R061_axios get 호출

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./App.css";
-import FetchPost from "./R060_FetchPost.js";
+import AxiosGet from "./R061_AxiosGet.js";
 // import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <FetchPost />
+      <AxiosGet />
     </div>
   );
 }
