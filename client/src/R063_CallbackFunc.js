834a694845fff66f8f0be5315b26e942971d05a8
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-05 21:13:29 +0900

    #R063_콜백 함수 사용

---
+++ client/src/R063_CallbackFunc.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class R063_CallbackFunc extends Component {
+  componentDidMount() {
+    this.logPrint(
+      1,
+      function (return1) {
+        console.log("return1 : " + return1);
+        this.logPrint(return1, function (return2) {
+          console.log("return2 : " + return2);
+        });
+      }.bind(this)
+    );
+  }
+
+  logPrint(param, callback) {
+    console.log("logPrint paran :" + param);
+    param += param;
+    callback(param);
+  }
+  render() {
+    return <h1>Callback function</h1>;
+  }
+}
+
+export default R063_CallbackFunc;
