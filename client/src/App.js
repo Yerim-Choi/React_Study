603ddae59883f6fe1dffa4c02e563e95101e4210
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-10 22:18:48 +0900

    #R068_mousemove 이벤트 사용(onMouseMove)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./App.css";
-import ReactChange from "./R067_onChange.js";
+import ReactMouseMove from "./R068_onMouseMove.js";
 // import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactChange />
+      <ReactMouseMove />
     </div>
   );
 }
