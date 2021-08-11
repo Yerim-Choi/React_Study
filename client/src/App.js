34ae4fa5fb5b2b5a6b9ad1cbefcd050d7629e9dd
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-11 23:17:30 +0900

    #R069_mouseover 이벤트 사용(onMouseOver)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./App.css";
-import ReactMouseMove from "./R068_onMouseMove.js";
+import ReactMouseOver from "./R069_onMouseOver.js";
 // import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactMouseMove />
+      <ReactMouseOver />
     </div>
   );
 }
