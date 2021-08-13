953fddbcd93c98dc6bc1ef36d3a79cf0127756c3
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-14 08:46:01 +0900

    #R072_submit 이벤트 사용(onSubmit)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import "./App.css";
-import ReactOnKey from "./R071_OnKey.js";
+import ReactonSubmit from "./R072_onSubmit.js";
 // import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactOnKey />
+      <ReactonSubmit />
     </div>
   );
 }
