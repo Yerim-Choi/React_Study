2a51d86db74a50252c44f76bbfbdd14297de801e
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-22 18:05:30 +0900

    #R079_redux 스토어 상속과 디스패치 함수 실행

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 // import "./App.css";
-import ContextApi from "./Context/R077_ContextApi.js";
+// import ContextApi from "./Context/R077_ContextApi.js";
 // import "bootstrap/dist/css/bootstrap.css";
+import StrAddButton from "./StrAddButton";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <ContextApi />
+      <span>{this.props.store.getState().data.str}</span>
+      <br />
+      <StrAddButton store={this.props.store} />
     </div>
   );
 }
