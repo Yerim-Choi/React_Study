005623a07926b4509562bc04e55b042b86a9d668
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-23 21:47:26 +0900

    #R080_redux 리듀서에서 스토어 데이터 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 // import "./App.css";
 // import ContextApi from "./Context/R077_ContextApi.js";
 // import "bootstrap/dist/css/bootstrap.css";
 import StrAddButton from "./StrAddButton";
 
-function App() {
-  return (
-    <div>
-      <h1>Start React 200!</h1>
-      <span>{this.props.store.getState().data.str}</span>
-      <br />
-      <StrAddButton store={this.props.store} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Start React 200!</h1>
+        <span>{this.props.store.getState().data.str}</span>
+        <br />
+        <StrAddButton store={this.props.store} />
+      </div>
+    );
+  }
 }
 
 export default App;
