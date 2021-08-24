4757537c7ac53e15f762f472f2c18e663cdf1568
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-24 22:19:28 +0900

    #R081_react-redux로 스토어 상속

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import reducers from "./reducers";
 
 const store = createStore(reducers);
 
 const listener = () => {
-  ReactDOM.render(<App store={store} />, document.getElementById("root"));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App store={store} />
+    </Provider>,
+    document.getElementById("root")
+  );
 };
 
 // If you want to start measuring performance in your app, pass a function
