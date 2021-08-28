3e2bfe9690896b6b7e9705f135d15b5974330757
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-28 23:52:23 +0900

    #R085_react-cookies save 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,15 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-// import "./App.css";
-// import ContextApi from "./Context/R077_ContextApi.js";
-// import "bootstrap/dist/css/bootstrap.css";
-import StrAddButton from "./StrAddButton";
+import CookieSave from "./R085_cookieSave.js";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Start React 200!</h1>
-        {/* <span>{this.props.store.getState().data.str}</span> */}
-        <span>{this.props.str}</span>
-        <br />
-        {/* <StrAddButton store={this.props.store} /> */}
-        <StrAddButton AppProp="200" />
+        <CookieSave />
       </div>
     );
   }
 }
-
-let mapStateToProps = (state, props) => {
-  console.log("Props from index.js : " + props.indexProp);
-  return {
-    str: state.data.str,
-  };
-};
-App = connect(mapStateToProps, null)(App);
 
 export default App;
