d7a08735286ea232ec7a15b819f2e733ba906d52
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-29 00:37:23 +0900

    #R086_react-cookies load 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import CookieSave from "./R085_cookieSave.js";
+import CookieLoad from "./R086_cookieLoad.js";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Start React 200!</h1>
-        <CookieSave />
+        <CookieLoad />
       </div>
     );
   }
