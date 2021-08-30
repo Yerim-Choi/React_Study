c09c55ee97cab641d3faea5cd78ab1f30e67357a
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-30 23:05:38 +0900

    #R087_react-cookies remove 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import CookieLoad from "./R086_cookieLoad.js";
+import CookieRemove from "./R087_cookieRemove.js";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Start React 200!</h1>
-        <CookieLoad />
+        <CookieRemove />
       </div>
     );
   }
