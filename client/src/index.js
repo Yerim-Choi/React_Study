f8ff504c4ce7dc28b57eba9b49e49feb4d752511
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-08-31 23:12:50 +0900

    #R088_react-router-dom BrowserRouter 사용

--- client/src/index.js
+++ client/src/index.js
@@ -1,45 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
-import { applyMiddleware, createStore } from "redux";
-import { Provider } from "react-redux";
-import reducers from "./reducers";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-const CallMiddleware = (store) => (nextMiddle) => (action) => {
-  console.log("1. reducer 실행 전");
-  console.log(
-    "2. action.type : " +
-      action.type +
-      ", store str : " +
-      store.getState().data.str
-  );
-  let result = nextMiddle(action);
-  console.log("3. reducer 실행 후");
-  console.log(
-    "4. action.type : " +
-      action.type +
-      ", store str : " +
-      store.getState().data.str
-  );
-  return result;
-};
-
-const store = createStore(reducers, applyMiddleware(CallMiddleware));
-
-const listener = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App indexProp="react" />
-    </Provider>,
-    document.getElementById("root")
-  );
-};
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-store.subscribe(listener);
-listener();
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
