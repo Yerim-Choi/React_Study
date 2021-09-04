a75dd2bbed979db6a2684386708e33acbc444ac2
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-09-04 23:08:57 +0900

    #R092_home 페이지 만들기

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import reactRouter from './R089_reactRouter'
-import reactRouter2 from './R089_reactRouter2'
 
 // css
-import '../css/new.css';
+import "../css/new.css";
 
 // header
-import HeaderAdmin from './Header/Header admin';
+import HeaderAdmin from "./Header/Header admin";
 
 // footer
-import Footer from './Footer/Footer';
+import Footer from "./Footer/Footer";
+
+// login
+import LoginForm from "./LoginForm";
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className="App">
-        <HeaderAdmin/> 
-        <Route exact path='/' component={reactRouter} />
-        <Route exact path='/reactRouter2' component={reactRouter2} />
-        <Footer/>
+        <HeaderAdmin />
+        <Route exact path="/" component={LoginForm} />
+        <Footer />
       </div>
     );
   }
