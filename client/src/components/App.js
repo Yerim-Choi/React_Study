5cb5dd979ed9ff9d464e32fed04f28a980887ced
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-09-28 22:22:12 +0900

    #R112_concurrently react, node 서버 구동

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,84 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 
 // css
-import "../css/new.css";
+import '../css/new.css';
 
 // header
-import HeaderAdmin from "./Header/Header admin";
+import HeaderAdmin from './Header/Header admin';
 
 // footer
-import Footer from "./Footer/Footer";
+import Footer from './Footer/Footer';
 
 // login
-import LoginForm from "./LoginForm";
+import LoginForm from './LoginForm';
 
-import reactDebounce from "./R094_reactDebounce";
-
-import reactThrottle from "./R095_reactThrottle";
-
-import floatingPopulationList from "./Floating_population/floatingPopulationList";
-
-import rechartsSimpleLineChart from "./Floating_population/rechartsSimpleLineChart";
-
-import floatingPopulationListChart from "./Floating_population/floatingPopulationListChart";
-
-import floatingPopulationAreaChart from "./Floating_population/floatingPopulationAreaChart";
-
-import floatingPopulationBarChart from "./Floating_population/floatingPopulationBarChart";
-import floatingPopulationComposedChart from "./Floating_population/floatingPopulationComposedChart";
-import floatingPopulationScatterChart from "./Floating_population/floatingPopulationScatterChart";
-import reactProxy from "./R109_reactProxy";
-import ApiGetJson from "./R110_ApiGetJson";
-import ApiPostJson from "./R111_ApiPostJson";
+import ApiPostJson from './R111_ApiPostJson';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <HeaderAdmin />
-        <Route exact path="/" component={LoginForm} />
-        <Route exact path="/Debounce" component={reactDebounce} />
-        <Route exact path="/Throttle" component={reactThrottle} />
-        <Route
-          exact
-          path="/floatingPopulationList"
-          component={floatingPopulationList}
-        />
-        <Route
-          exact
-          path="/rechartsSimpleLineChart"
-          component={rechartsSimpleLineChart}
-        />
-        <Route
-          exact
-          path="/floatingPopulationListChart"
-          component={floatingPopulationListChart}
-        />
-        <Route
-          exact
-          path="/floatingPopulationAreaChart"
-          component={floatingPopulationAreaChart}
-        />
-        <Route
-          exact
-          path="/floatingPopulationBarChart"
-          component={floatingPopulationBarChart}
-        />
-        <Route
-          exact
-          path="/floatingPopulationComposedChart"
-          component={floatingPopulationComposedChart}
-        />
-        <Route
-          exact
-          path="/floatingPopulationScatterChart"
-          component={floatingPopulationScatterChart}
-        />
-        <Route exact path="/reactProxy" component={reactProxy} />
-        <Route exact path="/ApiGetJson" component={ApiGetJson} />
-        <Route exact path="/ApiPostJson" component={ApiPostJson} />
-        <Footer />
+        <HeaderAdmin/> 
+        <Route exact path='/' component={LoginForm} />
+        <Route exact path='/ApiPostJson' component={ApiPostJson} />
+        <Footer/>
       </div>
     );
   }
--- client/src/components/R111_ApiPostJson.js
+++ client/src/components/R111_ApiPostJson.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
-const axios = require("axios");
+import React, { Component } from 'react';
+const axios = require('axios');
 
 class R111_ApiPostJson extends Component {
-  componentDidMount() {
-    axios.post("/users", {}).then((response) => {
-      console.log("response.data.message : " + response.data.message);
-    });
+  componentDidMount(){
+    axios.post('/users', {
+    })
+    .then( response => {
+        console.log("response.data.message : "+response.data.message)
+    })  
   }
-
+  
   render() {
-    return <h1>Call Node Api Post</h1>;
+    return (
+      <><h1>Call Node Api Post</h1></>
+    )
   }
 }
 
