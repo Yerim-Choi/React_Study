8d0aef78c1b6179a456ebc9136552752772c4e63
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-12-08 23:06:45 +0900

    #R158_로그인 회원 권한 허용 - 쿠키 회원정보 불러오기

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 var swtoolRouter = require("./routes/SwtoolRout");
 var fileuploadRouter = require("./routes/UploadRout");
 var usersRouter = require("./routes/UsersRout");
 
 var app = express();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use("/api/Swtool", swtoolRouter);
 app.use("/api/upload", fileuploadRouter);
 app.use(express.static("./uploads"));
