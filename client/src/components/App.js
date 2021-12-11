7837d831e492275e60ca937be37159406738e6e0
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-12-11 23:33:39 +0900

    #R160_로그인 상태 header 표시 및 로그아웃 구현

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,83 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import cookie from 'react-cookies';
+import cookie from "react-cookies";
 import axios from "axios";
 
 // css
-import '../css/new.css';
+import "../css/new.css";
 
 // header
-import HeaderAdmin from './Header/Header admin';
+import HeaderAdmin from "./Header/Header admin";
 
 // footer
-import Footer from './Footer/Footer';
+import Footer from "./Footer/Footer";
 
 // login
-import LoginForm from './LoginForm';
+import LoginForm from "./LoginForm";
 
-import SoftwareList from './SoftwareToolsManage/SoftwareList';
-import SoftwareView from './SoftwareToolsManage/SoftwareView';
+import SoftwareList from "./SoftwareToolsManage/SoftwareList";
+import SoftwareView from "./SoftwareToolsManage/SoftwareView";
 
-import Register from './Register/Register';
+import Register from "./Register/Register";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    }
+    this.state = {};
   }
 
   componentDidMount() {
-    axios.post('/api/LoginForm?type=SessionConfirm', {
-      token1 : cookie.load('userid') 
-      , token2 : cookie.load('username') 
-    })
-    .then( response => {
-        this.state.userid = response.data.token1
-        let password = cookie.load('userpassword')
-        if(password !== undefined){
-          axios.post('/api/LoginForm?type=SessionSignin', {
-            is_Email: this.state.userid,
-            is_Token : password
-          })
-          .then( response => {
-            if(response.data.json[0].useremail === undefined){
-              this.noPermission()
-            }
-          })
-          .catch( error => {
-            this.noPermission()
-          });
-        }else{
-          this.noPermission()
+    axios
+      .post("/api/LoginForm?type=SessionConfirm", {
+        token1: cookie.load("userid"),
+        token2: cookie.load("username"),
+      })
+      .then((response) => {
+        this.state.userid = response.data.token1;
+        let password = cookie.load("userpassword");
+        if (password !== undefined) {
+          axios
+            .post("/api/LoginForm?type=SessionSignin", {
+              is_Email: this.state.userid,
+              is_Token: password,
+            })
+            .then((response) => {
+              if (response.data.json[0].useremail === undefined) {
+                this.noPermission();
+              }
+            })
+            .catch((error) => {
+              this.noPermission();
+            });
+        } else {
+          this.noPermission();
         }
-    })
-    .catch( response => this.noPermission());
+      })
+      .catch((response) => this.noPermission());
   }
 
   noPermission = (e) => {
-    if(window.location.hash != 'nocookie'){
+    if (window.location.hash != "nocookie") {
       this.remove_cookie();
-      window.location.href = '/login/#nocookie';
+      window.location.href = "/login/#nocookie";
     }
   };
 
   remove_cookie = (e) => {
-    cookie.remove('userid', { path: '/'});
-    cookie.remove('username', { path: '/'});
-    cookie.remove('userpassword', { path: '/'});
-  }
+    cookie.remove("userid", { path: "/" });
+    cookie.remove("username", { path: "/" });
+    cookie.remove("userpassword", { path: "/" });
+  };
 
-  render () {
+  render() {
     return (
       <div className="App">
-        <HeaderAdmin/> 
-        <Route exact path='/' component={LoginForm} />
-        <Route path='/login' component={LoginForm} />
-        <Route path='/SoftwareList' component={SoftwareList} />
-        <Route path='/SoftwareView/:swtcode' component={SoftwareView} />
-        <Route path='/register' component={Register} />
-        <Footer/>
+        <HeaderAdmin />
+        <Route exact path="/" component={LoginForm} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/SoftwareList" component={SoftwareList} />
+        <Route path="/SoftwareView/:swtcode" component={SoftwareView} />
+        <Route path="/register" component={Register} />
+        <Footer />
       </div>
     );
   }
